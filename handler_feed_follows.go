0cefec73399189a40019397955f2aedd76be9314
iceblade92 <163899714+iceblade92@users.noreply.github.com>
2026-06-16 15:23:54 +0200

    unfollow command and sql deletefeedfollow

--- handler_feed_follows.go
+++ handler_feed_follows.go
@@ -52,3 +52,24 @@ func handlerFollowing(s *state, cmd command, user database.User) error {
 
 	return nil
 }
+
+func handlerUnfollow(s *state, cmd command, user database.User) error {
+	if len(cmd.Args) != 1 {
+		return fmt.Errorf("Need one argument for deleting feed")
+	}
+
+	url := cmd.Args[0]
+	feed, err := s.db.GetFeedByURL(context.Background(), url)
+	if err != nil {
+		return err
+	}
+
+	err = s.db.DeleteFeedFollow(context.Background(), database.DeleteFeedFollowParams{
+		UserID: user.ID,
+		FeedID: feed.ID,
+	})
+	if err != nil {
+		return err
+	}
+	return nil
+}
--- sql/queries/feed_follows.sql
+++ sql/queries/feed_follows.sql
@@ -37,3 +37,6 @@ INNER JOIN feeds
 INNER JOIN users
     ON feed_follows.user_id = users.id
 WHERE feed_follows.user_id = $1;
+
+-- name: DeleteFeedFollow :exec
+DELETE FROM feed_follows WHERE user_id = $1 AND feed_id = $2;
